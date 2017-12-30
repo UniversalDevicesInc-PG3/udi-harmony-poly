d92d0ec37f42edbe21c55b1985b1e3be06719cba
JimBo CA <jimboca3@gmail.com>
2017-12-29 17:18:49 -0800

    Start from template

---
+++ harmony_hub.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+This is the Harmony Hub NodeServer for ISY and Polyglot v2 written in Python2/3
+by JimBo jimboca3@gmail.com
+"""
+
+import polyinterface
+import sys
+"""
+Import the polyglot interface module. This is in pypy so you can just install it
+normally. Replace pip with pip3 if you are using python3.
+
+Virtualenv:
+pip install polyinterface
+
+Not Virutalenv:
+pip install polyinterface --user
+
+*I recommend you ALWAYS develop your NodeServers in virtualenv to maintain
+cleanliness, however that isn't required. I do not condone installing pip
+modules globally. Use the --user flag, not sudo.
+"""
+
+LOGGER = polyinterface.LOGGER
+"""
+polyinterface has a LOGGER that is created by default and logs to:
+logs/debug.log
+You can use LOGGER.info, LOGGER.warning, LOGGER.debug, LOGGER.error levels as needed.
+"""
+
+# Harmony Hub Modules
+from harmony_hub_nodes import HarmonyController
+from harmony_hub_version import VERSION_MAJOR,VERSION_MINOR
+
+if __name__ == "__main__":
+    try:
+        polyglot = polyinterface.Interface('MyNodeServer')
+        """
+        Instantiates the Interface to Polyglot.
+        """
+        polyglot.start()
+        """
+        Starts MQTT and connects to Polyglot.
+        """
+        control = Controller(polyglot)
+        """
+        Creates the Controller Node and passes in the Interface
+        """
+        control.runForever()
+        """
+        Sits around and does nothing forever, keeping your program running.
+        """
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit(0)
+        """
+        Catch SIGTERM or Control-C and exit cleanly.
+        """
---
+++ harmony_hub_version.py
@@ -0,0 +1,4 @@
+# This is the main version
+VERSION_MAJOR = 0.3
+# This is for pre-release or minor changes.
+VERSION_MINOR = 3.0
